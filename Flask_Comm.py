81f6d060c45b786d3aa8b0b196c6826348928549
Avinash Sharma <96874114+Avinash238@users.noreply.github.com>
2023-07-31 20:43:58 +0530

    Add files via upload

---
+++ Flask_Comm.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+from forex_python.converter import CurrencyRates
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    try:
+        from_currency = request.form['from_currency']
+        to_currency = request.form['to_currency']
+        amount = float(request.form['amount'])
+
+        c = CurrencyRates()
+        converted_amount = c.convert(from_currency, to_currency, amount)
+
+        return jsonify({'result': converted_amount})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
